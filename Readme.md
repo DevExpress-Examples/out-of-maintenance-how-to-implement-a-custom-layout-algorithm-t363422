5579295029e6e127849dfd9770febfab1994a875
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: Implement a custom layout algorithm
+
+
+This example demonstrates how to implement a custom layout algorithm, that can be used to place tree map items into a parent container and set their size.
+
+
+<h3>Description</h3>
+
+To do this, design a class implementing the&nbsp;<strong>ITreeMapLayoutAlgorithm</strong>&nbsp;interface and implement its&nbsp;<strong>Calculate</strong> method.
+
+<br/>
+
+
---
+++ VB/CustomLayoutAlgorithmSample/Form1.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Windows.Forms
+
+Namespace CustomLayoutAlgorithmSample
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            treeMap.LayoutAlgorithm = New CustomLayoutAlgorithm()
+        End Sub
+    End Class
+End Namespace
